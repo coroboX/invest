c047e4b0fc62d701709f8e4bcee1b46d0786d1d0
Yakiv Syvukha <yakov.sivukha@gmail.com>
2020-11-03 15:12:29 +0200

    split

---
+++ index.js
@@ -0,0 +1,51 @@
+function logEvent(eventName) {
+  if (window.Android) {
+      Android.logEvent(eventName);
+  }
+
+  if (
+      window.webkit &&
+      window.webkit.messageHandlers &&
+      window.webkit.messageHandlers.iosListener
+  ) {
+      window.webkit.messageHandlers.iosListener.postMessage(eventName);
+  }
+
+  window.parent.postMessage(eventName, '*')
+}
+
+var autofillEvent = document.createEvent('Event');
+autofillEvent.initEvent('fa.investnow.autofill', true, false);
+
+autofillEvent.investor = {
+    "type": 'person',
+    "name": '{{ $user->full_name }}',
+    "amount": '{{ $tier->value }}',
+    "email": '{{ $user->email }}',
+};
+
+var clearEvent = document.createEvent('Event');
+
+clearEvent.initEvent('fa.investnow.clear', true, false);
+
+// Invest Now button click
+window.onload = function() {
+    document.getElementById("invest-now-button").click();
+    document.dispatchEvent(clearEvent);
+};
+
+// Event listeners
+document.addEventListener('fa.investnow.close', function(e){
+    document.dispatchEvent(clearEvent);
+    logEvent('Close');
+});
+
+document.addEventListener('fa.investnow.open', function(e){
+    document.dispatchEvent(clearEvent);
+    document.dispatchEvent(autofillEvent);
+    logEvent('Open');
+});
+
+document.addEventListener('fa.investnow.success', function(e){
+    logEvent('Success')
+});
