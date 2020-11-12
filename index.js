3dd31a2193209ce45913ad72fca59fd5c50130fd
Yakiv Syvukha <yakov.sivukha@gmail.com>
2020-11-12 17:09:33 +0200

    gold progress-bar

--- index.js
+++ index.js
@@ -48,6 +48,13 @@ function seekAndHide() {
   const oldPrev = document.querySelector('button[ng-if="modal.showPrev()"]');
   const oldClear = document.querySelector('button[ng-if="modal.showClear()"]');
   const oldClose = document.querySelector('button[ng-click="modal.cancel()"]');
+  const progressBar = document.querySelector(`div[ng-style="{'width': modal.percentComplete() + '%' }"]`);
+
+  if (progressBar){
+    progressBar.style.backgroundImage = 'linear-gradient(to bottom, #D4AB72 0, #B19164 100%)';
+    progressBar.style.backgroundColor = '#D4AB72';
+    progressBar.style.color = "#000";
+  }
 
   if (oldNext && !oldNext.classList.contains('hide-button')) {
     hideButton(oldNext);
